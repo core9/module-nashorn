fd2c725a18a52cdab96dad759da0475338f838b7
Robert Cooke <robert.cooke@trimm.nl>
2014-05-19 08:25:53 +0200

    adding setJson getJson on JavascriptModule interface

--- src/impl/java/io/core9/plugin/nashorn/JavascriptModule.java
+++ src/impl/java/io/core9/plugin/nashorn/JavascriptModule.java
@@ -1,7 +1,11 @@
 package io.core9.plugin.nashorn;
 
+import net.minidev.json.JSONObject;
+
 public interface JavascriptModule {
 
 	String getName();
 	void setName(String name);
+	JSONObject getJson();
+	void setJson(JSONObject json);
 }
--- src/impl/java/io/core9/plugin/nashorn/JavascriptModuleImpl.java
+++ src/impl/java/io/core9/plugin/nashorn/JavascriptModuleImpl.java
@@ -1,15 +1,29 @@
 package io.core9.plugin.nashorn;
 
+import net.minidev.json.JSONObject;
+
 public class JavascriptModuleImpl implements JavascriptModule{
 
 	private String name;
+	private JSONObject json;
 	
 	@Override
 	public String getName() {
 		return name;
 	}
 
+	@Override
 	public void setName(String name){
 		this.name = name;
+	}
+
+	@Override
+	public JSONObject getJson() {
+		return json;
+	}
+
+	@Override
+	public void setJson(JSONObject json) {
+		this.json = json;
 	}
 }
