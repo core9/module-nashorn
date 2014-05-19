0fdf847d0a555331af27b25422b25db3f84ef60f
Robert Cooke <robert.cooke@trimm.nl>
2014-05-19 16:18:58 +0200

    refactor

--- src/impl/java/io/core9/plugin/javascript/JavascriptDataHandlerImpl.java
+++ src/impl/java/io/core9/plugin/javascript/JavascriptDataHandlerImpl.java
@@ -1,25 +1,15 @@
 package io.core9.plugin.javascript;
 
-
 import io.core9.plugin.filesmanager.FileRepository;
 import io.core9.plugin.server.request.Request;
 import io.core9.plugin.server.vertx.VertxServer;
 import io.core9.plugin.widgets.datahandler.DataHandler;
 import io.core9.plugin.widgets.datahandler.DataHandlerFactoryConfig;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
-
-import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
-
-import com.google.common.io.ByteStreams;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
@@ -44,14 +34,12 @@ public class JavascriptDataHandlerImpl implements
 		return "Javascript";
 	}
 
-	private NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
-	// secure
-	private ScriptEngine sengine = factory
-			.getScriptEngine(new String[] { "--no-java" });
+	
+	private JavascriptExecutor jsExecutor = JavascriptExecutor.getInstance();
+	
+	private Map<String, Object> result = new HashMap<String, Object>();
 
-	private Map<String, Object> resultRegistry = new HashMap<>();
 
-	private String js;
 
 	@Override
 	public Class<? extends DataHandlerFactoryConfig> getConfigClass() {
@@ -64,35 +52,34 @@ public class JavascriptDataHandlerImpl implements
 			final DataHandlerFactoryConfig options) {
 		return new DataHandler<JavascriptDataHandlerConfig>() {
 
-			private Map<String, Object> result = new HashMap<String, Object>();
+			
 
 			@Override
 			public Map<String, Object> handle(Request req) {
 
+				
+				jsExecutor.setFileRepository(repository);
+				
 				JSONObject configuration = null;
 				Map<String, Object> nashorn = new HashMap<String, Object>();
 				Map<String, Object> file = getJsFile(options, req);
-				JSONObject server = getServerObject();
-				evalJavascript(file);
+				JSONObject server = jsExecutor.getServerObject();
+				 String js = jsExecutor.evalJavascript(file);
 
 				ConfigReader config = new ConfigReader();
 
 				config.setJavascript(js);
-	
 
 				if (config.hasConfig()) {
 					configuration = config.getConfiguration();
 					for (Entry<String, Object> param : configuration.entrySet()) {
 
 						if (param.getKey().equals("include")) {
-							importFiles((JSONArray) param.getValue(), req);
+							jsExecutor.importFiles((JSONArray) param.getValue(), req);
 						}
 
-						System.out
-								.println(System.getProperty("line.separator"));
-
 						if (param.getKey().equals("var")) {
-							execVars((JSONArray) param.getValue());
+							nashorn = jsExecutor.execVars((JSONArray) param.getValue());
 						}
 
 					}
@@ -102,25 +89,9 @@ public class JavascriptDataHandlerImpl implements
 				return result;
 			}
 
-			private JSONObject getServerObject() {
-				JSONObject server = new JSONObject();
-				JSONObject request = new JSONObject();
-				request.put("path", "/nashorn");
-				server.put("request", request);
-				return server;
-			}
 
-			private String getContentFromFile(Map<String, Object> file) {
-				String js = "";
-				try {
-					js = new String(ByteStreams.toByteArray((InputStream) file
-							.get("stream")));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
 
-				return js;
-			}
+
 
 			private Map<String, Object> getJsFile(
 					final DataHandlerFactoryConfig options, Request req) {
@@ -134,139 +105,9 @@ public class JavascriptDataHandlerImpl implements
 			}
 		};
 	}
+	
+	
+	
 
-	private void evalJavascript(Map<String, Object> file) {
-		try {
-			js = new String(ByteStreams.toByteArray((InputStream) file
-					.get("stream")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			sengine.eval(js);
-		} catch (ScriptException e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
-
-	private void execVars(JSONArray vars) {
-		System.out.println(vars);
-		String name = "";
-		String invoked = "";
-		String arg = null;
-		Object res = null;
-		String invokeObject = "";
-
-		Object object = null;
-
-		for (Object var : vars) {
-
-			JSONObject obj = (JSONObject) var;
-
-			name = (String) obj.get("name");
-			System.out.println(name);
-			invoked = (String) obj.get("invoke");
-
-			String[] invoke = invoked.split("\\.");
-			if (invoke.length > 1) {
-				invokeObject = invoke[0];
-				invoked = invoke[1];
-			}
-
-			System.out.println(invoked);
-			arg = (String) obj.get("arg");
-			System.out.println(arg);
-
-			Invocable invocable = (Invocable) sengine;
-
-			if (invokeObject != null) {
-
-				try {
-					object = sengine.eval(invokeObject);
-				} catch (ScriptException e) {
-					e.printStackTrace();
-				}
-				res = invokeFunctionOnObject(object, invoked, arg, res,
-						invocable);
-			} else {
-				res = invokeFunction(invoked, arg, res, invocable);
-			}
-
-			invokeObject = null;
-			object = null;
-
-			resultRegistry.put(name, res);
-			System.out.println("Results : " + res);
-
-		}
-
-	}
-
-	private Object invokeFunctionOnObject(Object object, String invoked,
-			String arg, Object res, Invocable invocable) {
-
-		try {
-			String args = (String) resultRegistry.get(arg);
-			if (args == null) {
-				res = invocable.invokeMethod(object, invoked);
-			} else {
-				res = invocable.invokeMethod(object, invoked, args);
-			}
-
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (ScriptException e) {
-			e.printStackTrace();
-		}
-		return res;
-	}
-
-	private Object invokeFunction(String invoked, String arg, Object res,
-			Invocable invocable) {
-		try {
-			String args = (String) resultRegistry.get(arg);
-			res = invocable.invokeFunction(invoked, args);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (ScriptException e) {
-			e.printStackTrace();
-		}
-		return res;
-	}
-
-	private void importFiles(JSONArray files, Request req) {
-
-		for (Object file : files) {
-
-
-
-			Map<String, Object> fileObj = repository.getFileContentsByName(
-					req.getVirtualHost(), (String) file);
-
-			localEvalJavascript(fileObj);
-
-		}
-	}
-
-	private void localEvalJavascript(Map<String, Object> fileObj) {
-		String js = "";
-		try {
-			js = new String(ByteStreams.toByteArray((InputStream) fileObj
-					.get("stream")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			sengine.eval(js);
-		} catch (ScriptException e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
-
+	
 }
